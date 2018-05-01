576232e1cb21207cf2e285ba4e4ba83aa1d2f3fa
Vadim Feshchenko <vadim0961267452@gmail.com>
2018-05-02 00:50:39 +0300

    Add Flask routes for mapping CRUD operarations

--- src/app.py
+++ src/app.py
@@ -1,18 +1,90 @@
-from flask import Flask, jsonify
-import random
+import random as rnd
+from flask import Flask
+from flask import abort
+from flask import jsonify
+from flask import make_response
+from flask import request
+from flask import url_for
 
+from data_provider_service import DataProviderService
 from quotes import funny_quotes
 
+DATA_PROVIDER = DataProviderService(15)
 app = Flask(__name__)
 
 
-@app.route("/api/funny")
-def serve_funny_quotes():
+@app.route('/api', methods=['GET'])
+def list_routes():
+    result = []
+    for rt in app.url_map.iter_rules():
+        result.append({
+            'methods': list(rt.methods),
+            'route': str(rt)
+        })
+    return jsonify({'routes': result, 'total': len(result)})
+
+
+def candidate():
+    candidates = DATA_PROVIDER.get_candidates()
+    return jsonify({'candidates': candidates, 'total': len(candidates)})
+
+
+app.add_url_rule('/api/candidate', 'candidate', candidate)
+
+
+@app.route('/api/candidate/<string:id>', methods=['GET'])
+def candidate_by_id(id):
+    candidate = DATA_PROVIDER.get_candidate(id)
+    if candidate:
+        return jsonify({'candidate': candidate})
+    else:
+        abort(404)
+
+
+@app.route('/api/candidate<string:id>/name/<string:new_name>', methods=['PUT'])
+def update_name(id, new_name):
+    nr_of_updated_items = DATA_PROVIDER.update_name(id, new_name)
+    if nr_of_updated_items == 0:
+        abort(404)
+    else:
+        return jsonify({'total_updated': nr_of_updated_items})
+
+
+@app.route('/api/random/candidate', defaults={'nrOfItems': 1}, methods=['GET'])
+@app.route('/api/random/candidate/<int:nrOfItems>', methods=['GET'])
+def random(nrOfItems):
+    candidates = DATA_PROVIDER.get_random_candidates(nrOfItems)
+    return jsonify({'candidates': candidates, 'total': len(candidates)})
+
+
+@app.route('/api/candidate/delete/<string:id>', methods=['DELETE'])
+def delete(id):
+    if (DATA_PROVIDER.delete_candidate(id)):
+        return make_response('', 200)
+    else:
+        return abort(404)
+
+
+@app.route('/api/candidate', methods=['POST'])
+def add_candidate():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+
+    new_camdidate_id = DATA_PROVIDER.add_candidate(first_name, last_name)
+
+    return jsonify({
+        'id': new_camdidate_id,
+        'url': url_for('candidate_by_id', id=new_camdidate_id)
+    })
+
+
+@app.route('/api/funny')
+def serve_funny_quote():
     quotes = funny_quotes()
     nr_of_quotes = len(quotes)
-    selected_quote = quotes[random.randint(0, nr_of_quotes - 1)]
+    selected_quote = quotes[rnd.randint(0, nr_of_quotes - 1)]
     return jsonify(selected_quote)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- src/datagenerator.py
+++ src/datagenerator.py
@@ -1,12 +1,11 @@
-import random
 import datetime
+import random
 
 from candidate import Candidate
 from project import Project
-from experience import Experience
+
 
 class DataGenerator:
-
     __first_names = ['Alene', 'Alison', 'Alessandra', 'Aleta', 'Alethea', 'Alex', 'Alexa', 'Alexandra', 'Anastasia',
                      'Bob', 'Deedee', 'Denna', 'Deerdre', 'Deeyn', 'Dehlia', 'Deidre', 'Deina', 'Duke', 'Dom', 'Jane',
                      'John', 'Jennifer', 'Scott', 'Greg']
@@ -28,7 +27,7 @@ class DataGenerator:
             candidate = Candidate(rnd_fn[x], rnd_ln[x], experience)
             result.append(candidate)
 
-        return [ cand.serialize() for cand in result ]
+        return [cand.serialize() for cand in result]
 
     def generate_names(self, nr):
         rnd_fn = self.generate_first_names(nr)
@@ -39,24 +38,24 @@ class DataGenerator:
 
     def generate_first_names(self, nr):
         fn_length = len(self.__first_names)
-        result = [self.__first_names[random.randint(0, fn_length-1)] for x in range(nr)]
+        result = [self.__first_names[random.randint(0, fn_length - 1)] for x in range(nr)]
         return result
 
     def generate_last_names(self, nr):
         ln_length = len(self.__last_names)
-        result = [self.__last_names[random.randint(0, ln_length-1)] for x in range(nr)]
+        result = [self.__last_names[random.randint(0, ln_length - 1)] for x in range(nr)]
         return result
 
-    def generate_experience(self, nr, serialize = False):
+    def generate_experience(self, nr, serialize=False):
         result = []
         for x in range(nr):
             if random.randint(0, 1244) % 2 == 0:
-                project_name = '[New] Project ' + self.__first_names[random.randint(0, len(self.__first_names)-1)]
+                project_name = '[New] Project ' + self.__first_names[random.randint(0, len(self.__first_names) - 1)]
             else:
-                project_name = 'Project ' + self.__last_names[random.randint(0, len(self.__last_names)-1)]
+                project_name = 'Project ' + self.__last_names[random.randint(0, len(self.__last_names) - 1)]
 
             start_date = datetime.datetime(random.randint(1990, 2018), random.randint(1, 12), random.randint(1, 28))
-            end_date = start_date + datetime.timedelta(days=random.randint(2,432))
+            end_date = start_date + datetime.timedelta(days=random.randint(2, 432))
             prj = Project(project_name, start_date, end_date, 'Description for project ' + project_name)
             result.append(prj)
 
